250b432295b0e3e4d96819afb405e6c210602ff3
Eduardo Malherbi <emalherbi@gmail.com>
2018-05-30 08:56:43 -0300

    fix #9, #10

--- README.md
+++ README.md
@@ -29,20 +29,9 @@ try {
     ), realpath(dirname(__FILE__)));
 
     $isConnect = $mssql->isConnect();
-
-    $result = $mssql->execute("IF OBJECT_ID('USUARIOS') IS NULL
-        BEGIN
-            CREATE TABLE [dbo].[USUARIOS]
-            (
-                [ID_USUARIOS] [INT] IDENTITY(1,1) NOT NULL,
-                [NOME] VARCHAR(100) NOT NULL,
-                PRIMARY KEY CLUSTERED
-            ([ID_USUARIOS] ASC) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-            ) ON [PRIMARY]
-        END");
     echo '<pre>';
-    echo print_r($result);
-    echo '</pre>';    
+    echo print_r($isConnect);
+    echo '</pre>';   
 
     $row = $mssql->fetchRow('SELECT * FROM CLIENTES');
     echo '<pre>';
@@ -68,28 +57,40 @@ try {
 
     $mssql->commit();
 
-    $mssqlsx = new MyMssql\MyMssqlSx();
-
     $sxName = 'SX_CLIENTES';
     $params = array(1, 385);
-    $result = $mssqlsx->fetchRowSx($sxName, $params);
+    $result = $mssql->fetchRowSx($sxName, $params);
     echo '<pre>';
     echo print_r($result);
     echo '</pre>';
 
     $sxName = 'SX_CLIENTES';
     $params = array(2, 385);
-    $result = $mssqlsx->fetchAllSx($sxName, $params);
+    $result = $mssql->fetchAllSx($sxName, $params);
     echo '<pre>';
     echo print_r($result);
     echo '</pre>';
 
     $sxName = 'SX_CLIENTES_SAVE';
     $params = array(2, '2017-01-01', 385, 0, 0, 0, 0);
-    $result = $mssqlsx->execSx($sxName, $params);
+    $result = $mssql->execSx($sxName, $params);
     echo '<pre>';
     echo print_r($result);
     echo '</pre>';
+
+    $result = $mssql->execScript("IF OBJECT_ID('USUARIOS') IS NULL
+        BEGIN
+            CREATE TABLE [dbo].[USUARIOS]
+            (
+                [ID_USUARIOS] [INT] IDENTITY(1,1) NOT NULL,
+                [NOME] VARCHAR(100) NOT NULL,
+                PRIMARY KEY CLUSTERED
+            ([ID_USUARIOS] ASC) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+            ) ON [PRIMARY]
+        END");
+    echo '<pre>';
+    echo print_r($result);
+    echo '</pre>';    
 
     echo 'Success...';
 } catch (Exception $e) {
--- src/MyMssql.php
+++ src/MyMssql.php
@@ -142,6 +142,11 @@ class MyMssql
         return empty($result) ? false : $result[0];
     }
 
+    public function fetchRowSx($sxName, $params, $test = false)
+    {
+        return $this->querySx($sxName, $params, $test, 'fetchRow');
+    }
+
     public function fetchAll($sql)
     {
         $query = $this->query($sql);
@@ -156,6 +161,11 @@ class MyMssql
         }
 
         return empty($result) ? false : $result;
+    }
+
+    public function fetchAllSx($sxName, $params, $test = false)
+    {
+        return $this->querySx($sxName, $params, $test, 'fetchAll');
     }
 
     public function exec($sql)
@@ -175,7 +185,12 @@ class MyMssql
         }
     }
 
-    public function execute($sql)
+    public function execSx($sxName, $params, $test = false)
+    {
+        return $this->querySx($sxName, $params, $test, 'exec');
+    }
+
+    public function execScript($sql)
     {
         try {
             $this->connect();
@@ -187,9 +202,6 @@ class MyMssql
             $stmt = $this->db->prepare($sql);
             $stmt = $stmt->execute();
 
-            // fix #7
-            // Inserido a função "disconnect" para correção de erro quando executa muitos scripts ao mesmo tempo.
-            // General error: 10038 Attempt to initiate a new SQL Server operation with results pending.
             $this->disconnect();
 
             return $stmt;
@@ -254,6 +266,44 @@ class MyMssql
         return parse_ini_file($this->RT.$this->DS.'MyMssql.ini');
     }
 
+    private function querySx($sxName, $params, $test = false, $function = 'exec')
+    {
+        if (false === $this->sxExist($sxName)) {
+            return 'Stored Procedure '.$sxName.' not find.';
+        }
+
+        $array = $this->getFields($sxName);
+
+        if (count($params) !== count($array)) {
+            return 'Parameters reported differently than stored procedure parameters.';
+        }
+
+        $sql = $sxName.' ';
+
+        for ($i = 0; $i < count($params); ++$i) {
+            if (0 !== $i) {
+                $sql .= ', ';
+            }
+
+            $type = strtoupper($array[$i]['TYPE']);
+
+            if (in_array($type, array('DATETIME', 'SMALLDATETIME', 'TIMESTAMP', 'CHAR', 'NCHAR', 'SQLCHAR', 'TEXT', 'NTEXT', 'VARCHAR', 'NVARCHAR', 'SQLVARCHAR', 'BINARY', 'VARBINARY', 'IMAGE'), true)) {
+                $sql .= "'".$params[$i]."'";
+            } else {
+                $sql .= $params[$i];
+            }
+        }
+
+        if (true === $test) {
+            echo '<pre>';
+            echo print_r($sql);
+            echo '</pre>';
+            exit;
+        }
+
+        return $this->$function($sql);
+    }
+
     private function query($sql)
     {
         try {
@@ -278,6 +328,40 @@ class MyMssql
         }
 
         INI::write($this->RT.$this->DS.'MyMssql.ini', array('INI' => $ini));
+    }
+
+    private function sxExist($sxName)
+    {
+        $sql = '';
+        $sql .= ' SELECT COUNT(*) AS EXIST ';
+        $sql .= ' FROM SYSOBJECTS ';
+        $sql .= " WHERE ID = OBJECT_ID('".$sxName."')";
+
+        $array = parent::fetchRow($sql);
+        $count = $array['EXIST'];
+
+        return ($count > 0) ? true : false;
+    }
+
+    private function getFields($sxName)
+    {
+        $sql = '';
+        $sql .= ' SELECT ';
+        $sql .= ' COLUMNS.NAME AS COLUMNS, ';
+        $sql .= ' TYPES.NAME AS TYPE, ';
+        $sql .= ' COLUMNS.ISOUTPARAM, ';
+        $sql .= ' COLUMNS.LENGTH ';
+        $sql .= ' FROM ';
+        $sql .= ' SYSOBJECTS AS TABLES, ';
+        $sql .= ' SYSCOLUMNS AS COLUMNS, ';
+        $sql .= ' SYSTYPES AS TYPES ';
+        $sql .= ' WHERE ';
+        $sql .= ' TABLES.ID = COLUMNS.ID ';
+        $sql .= ' AND COLUMNS.USERTYPE = TYPES.USERTYPE ';
+        $sql .= " AND TABLES.NAME = '".$sxName."'";
+        $sql .= ' ORDER BY TABLES.ID ';
+
+        return parent::fetchAll($sql);
     }
 
     private function logger($str, $err = '')
